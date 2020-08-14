4221c36910d15aaf61fd6738a8f76160a06ab70d
Stone <tongshi@ucsb.edu>
2020-08-14 08:06:10 +0800

    8

--- R/day-07.R
+++ R/day-07.R
@@ -3,20 +3,20 @@
 #To visualize the COVID-19 cases data
 
 #the function to build a data frame of regions with cases and death cases
-library(tidyverse) %>%
-covid = read_csv("D:/LifeInUCSB/Study/GEOG176A/week2/assignment5/geog176A-daily-exercises/data/covid.csv") %>%
-state = read_csv("D:/LifeInUCSB/Study/GEOG176A/week2/assignment5/geog176A-daily-exercises/data/covid.csv")
+library(tidyverse)
+covid = read_csv("data/covid.csv") %>%
+  mutate(date = as.Date(date))
+region = data.frame(state = state.name, region = state.region)
+
 covid %>%
-  select(state,cases) %>%
-  region = data.frame(state = state.name, region = state.region)%>%
-    right_join(covid,region, by = "state")%>%
+    right_join(region, by = "state")%>%
     group_by(region,date)%>%
     summarize(cases  = sum(cases), deaths = sum(deaths))%>%
     ungroup()%>%
     pivot_longer(cols = c('cases', 'deaths')) ->
     regions_cases_deaths#pivot the frame to long
 
-ggplot(data=regions_cases_deaths, aes(x =as.Date(date), y = value, color = state)) +
+p1 = ggplot(data=regions_cases_deaths, aes(x =as.Date(date), y = value, color = region)) +
            geom_line(size = 1) +
            facet_wrap(~region) +
            ggthemes::theme_gdocs() +
@@ -25,7 +25,8 @@ ggplot(data=regions_cases_deaths, aes(x =as.Date(date), y = value, color = state
                 subtitle = "COVID-19 Data: NY-Times",
                 x = "Date",
                 y = "Daily Cummulative Count",
-                caption = "Daily Exercise 07")+
-           ggsave(file = "img/cummulative_regions.png",
-                  units = c("in"))
+                caption = "Daily Exercise 07")
+
+           ggsave(p1, file = "img/cummulative_regions.png",
+                  units = c("in"), width= 8)
 
